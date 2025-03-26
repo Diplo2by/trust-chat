1952b6d18fdb15d4340236886b80d7df8e133b2f
Diplo2by <contactdarshanv@gmail.com>
2025-03-26 16:29:51 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+function App() {
+
+  return (
+    <div className=" flex items-center justify-center h-full bg-green-400">
+      Hey Darsh
+    </div>
+  );
+}
+
+export default App;
